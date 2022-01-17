89bb7880de8d2b337d1862559797e4ab2e14c9c3
AndriiBoichuk <86463975+AndriiBoichuk@users.noreply.github.com>
2022-01-17 21:50:25 +0200

    Initial commit

---
+++ RestaurantsExplorer/Manager/AnswerManager.swift
@@ -0,0 +1,14 @@
+//
+//  AnswerManager.swift
+//  RestaurantsExplorer
+//
+//  Created by Андрій Бойчук on 17.01.2022.
+//
+
+import Foundation
+
+class AnswerManager {
+    
+    func getRestaurants(in city: String, category: )
+    
+}
